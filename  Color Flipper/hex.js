abf7958ceee6ff8f48f1b8833c3c634146868d99
thetoy0319 <pavandave0319@gmail.com>
2024-01-31 18:25:21 +0545

    formate code

---  Color Flipper/hex.js
+++  Color Flipper/hex.js
@@ -1,18 +1,18 @@
-const hex = [0,1,2,3,4,5,6,7,8.9,'a','b','c','d','e','f'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8.9, "a", "b", "c", "d", "e", "f"];
 
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-btn.addEventListener("click",function(){
-let hexColor = "#";
+btn.addEventListener("click", function () {
+  let hexColor = "#";
 
-for (i = 0; i<6 ;i++){
+  for (i = 0; i < 6; i++) {
     hexColor += hex[genretNumber()];
-}
-color.textContent = hexColor;
-document.body.style.backgroundColor = hexColor;
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
 });
 
-function genretNumber (){
-    return Math.floor(Math.random()*hex.length);
+function genretNumber() {
+  return Math.floor(Math.random() * hex.length);
 }
