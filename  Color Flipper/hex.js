34a2839ba8b983d56bff6242212efdbb09557a27
thetoy0319 <pavandave0319@gmail.com>
2024-01-31 17:37:58 +0545

    add addEventListener cange hadcoded value

---
+++  Color Flipper/hex.js
@@ -0,0 +1,14 @@
+const hex = [0,1,2,3,4,5,6,7,8.9,'a','b','c','d','e','f'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener("click",function(){
+let hexColor = "#";
+
+for (i = 0; i<6 ;i++){
+    hexColor += hex[0];
+}
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor;
+})
